58760fc08d0b27e46b0b546b46b6a5491732df8b
David Move <davidalmove@outlook.com>
2021-07-15 14:00:03 -0500

    Intial commit
    yolo

---
+++ sistema/build/web/js/js/menu.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+    //iniciar el dropdown del menu
+    $('select').formSelect();
+    $('.modal').modal();
+    $(".dropdown-trigger").dropdown({constrainWidth: false});
+    $('.sidenav').sidenav();
+    document.getElementById('foto_perfil').src = "http://gruposicsa.com/fotos/" + id_usuario + ".jpg";
+    $(".sidenav").empty();
+    // funcion que pinta el menu
+    for (indice in menu) {
+        $(".sidenav").append('<li><a onclick="verSubmenu(' + indice + ')"><i class="material-icons white-icon tooltipped" data-position="right"  data-tooltip="' + menu[indice].name + '">' + menu[indice].icono + '</i></a></li>');
+    }
+    $('.tooltipped').tooltip({margin: 20});
+    $("#info_gestor").fadeIn(1500);
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+        container: "body",
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        autoClose: true,
+        closeOnSelect: true, // Close upon selecting a date,
+        format: "yyyy-mm-dd"
+    });
+    $('.timepicker').timepicker({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        canceltext: 'Cancel', // Text for cancel-button,
+        container: "body", // ex. 'body' will append picker to body
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function () {} //Function for after opening timepicker
+    });
+    $('select').formSelect();
+
+});
+
+
+//funcion cerrar sesion con boton
+$("#cerrar").click(function () {
+    Cerrar();
+});
+
+//actualizar el estatus de f_logeado al cerar pestaña
+function Cerrar() {
+    $.ajax({
+        type: "POST",
+        url: "/sistema/ControllerUsuario",
+        data: {action: "cerrar_sesion", id_usuario: id_usuario}
+    });
+}
+
+// funcion que lanza el menu lateral
+$(".img_log").click(function () {
+    $('.sidenav').sidenav('open');
+});
+
+// funcion que pinta los submenus al dar click
+function verSubmenu(indice) {
+    $("#contenido").empty();
+    for (row in menu[indice].submenus) {
+        $("#contenido").append('<div class="col s6 m4 l4">' +
+                '<div class="col s10 offset-s1 padding_submenus" >' +
+                '<div class="s12 center-align background_submenus_title">' + menu[indice].submenus[row] + '</div>' +
+                '<div class="s12 center-align background_submenus"><a href="' + menu[indice].jsp[row] + '"><i class="medium material-icons white-icon">' + menu[indice].iconosSubmenus[row] + '</i></a><br></div>' +
+                '</div>' +
+                '</div>'
+                );
+    }
+}
+
+// Funciones para rl buscador
+$("#filtro").click(function () {
+    $("#resultado_menu").removeClass("hide");
+    $("#colect").empty();
+
+    for (p in menu) {
+        for (r in menu[p].submenus) {
+            $("#colect").append('<a href="' + menu[p].jsp[r] + '" class="collection-item">' + menu[p].submenus[r] + '</a>');
+        }
+    }
+});
+
+function myFunction_buscar() {
+    var query = $("#filtro").val();
+    var li = document.querySelectorAll('#resultado_menu div a');
+    for (var i = 0; i < li.length; i++) {
+        var a = li[i];
+        if (a.textContent.toLowerCase().indexOf(query.toLowerCase().trim()) > -1) {
+            a.style.display = "";
+        } else {
+            a.style.display = "none";
+        }
+    }
+}
+// funcion que esconde el menu 
+$("#colect").delegate('.collection-item', 'click', function () {
+    $("#resultado_menu").addClass("hide");
+});
+
+$(".row").click(function () {
+    $("#resultado_menu").addClass("hide");
+});
---
+++ sistema/build/web/main.jsp
@@ -0,0 +1,39 @@
+<%@page import="callcenter.model.ModelUsuario"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    int id_perfil = -1;
+    int id_usuario = -1;
+    String password = "-1";
+    String puesto = " ", menu = " ";
+    if (request.getParameter("id_usuario") == null || request.getParameter("id_usuario").isEmpty()
+            || request.getParameter("password") == null || request.getParameter("password").isEmpty()) {
+        response.sendRedirect("index.jsp");
+    } else {
+        id_perfil = Integer.parseInt(request.getParameter("id_perfil"));
+        id_usuario = Integer.parseInt(request.getParameter("id_usuario"));
+        password = request.getParameter("password");
+        // llamadas a metodos
+        menu = ModelUsuario.cargar_menu(id_perfil, id_usuario);
+        String id_puesto = ModelUsuario.id_puesto(id_usuario);
+        String id_puesto2 = ModelUsuario.id_puesto2(id_usuario);
+        String id_puesto3 = ModelUsuario.id_puesto3(id_usuario);
+        puesto = ModelUsuario.jsp_puesto(id_usuario);
+        
+        
+        // se inicia una sesion para el usuario;
+        HttpSession sesion = request.getSession();
+        // sele empiezan a agregar atributos al la variable de sesion
+        sesion.setAttribute("id_puesto", id_puesto);
+        sesion.setAttribute("id_puesto2", id_puesto2);
+        sesion.setAttribute("id_puesto3", id_puesto3);
+        sesion.setAttribute("id_usuario", id_usuario);
+        sesion.setAttribute("menu", menu);
+        sesion.setAttribute("puesto", puesto);
+        
+        // System.out.println(id_puesto + " \n" + id_usuario + " \n" + menu + " \n" + puesto + " \n" );
+
+    }
+%>
+<script>
+    location.replace("<%=puesto%>" + ".jsp");
+</script>
